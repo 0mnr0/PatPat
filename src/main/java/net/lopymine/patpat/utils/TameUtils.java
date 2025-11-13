580840d469b04a287558d5f3437281958676392f
JustDev <savelijln0@gmail.com>
2025-11-13 21:56:38 +0400

    Forgot to add heal for cats
    (Release has been re-uploaded)

--- src/main/java/net/lopymine/patpat/utils/TameUtils.java
+++ src/main/java/net/lopymine/patpat/utils/TameUtils.java
@@ -4,8 +4,9 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.animal.wolf.Wolf;
 
 import java.util.Random;
 import java.util.UUID;
@@ -20,22 +21,25 @@ public class TameUtils {
 
     public static void runByChance(Entity entity, ServerPlayer player, ServerLevel serverWorld) {
 
-        if (entity instanceof Wolf wolf) {
+        if (entity instanceof net.minecraft.world.entity.animal.wolf.Wolf wolf) {
             if (!wolf.isTame() && chance(wolf.isAngry() ? 0.25 : 1)) {
                 wolf.tame(player);
-                wolf.setOwnerUUID(player.getUUID());
+                wolf.setOwner(player);
                 wolf.stopBeingAngry(); // Because sounds are still appearing
                 showParticles(wolf, serverWorld);
             }
 
-            UUID PlayerTamed = wolf.getOwnerUUID();
+            LivingEntity PlayerTamedEntity = wolf.getOwner();
+            UUID PlayerTamed = null;
+            if (PlayerTamedEntity != null) {PlayerTamed = PlayerTamedEntity.getUUID();}
+
             if (chance(4) && PlayerTamed == player.getUUID()) {
                 final float wolfHp = wolf.getHealth();
                 float maxHealth = wolf.getMaxHealth();
                 float futureHp = wolfHp + 1f;
                 if (maxHealth < futureHp) {futureHp = maxHealth; }
                 wolf.setHealth(futureHp);
-                showParticles(wolf, serverWorld);
+                showParticles(wolf, serverWorld, 1);
             }
         }
 
@@ -43,10 +47,23 @@ public class TameUtils {
         if (entity instanceof Cat cat) {
             // It seems to me that in real life cats will be much less likely to become attached to a person if they are simply petted, but this is purely my opinion!
 
-            if (!cat.isTame() && chance(1.5)) {
+            if (!cat.isTame() && chance(0.25)) {
                 cat.tame(player);
-                cat.setOwnerUUID(player.getUUID());
+                cat.setOwner(player);
                 showParticles(cat, serverWorld);
+            }
+
+            LivingEntity PlayerTamedEntity = cat.getOwner();
+            UUID PlayerTamed = null;
+            if (PlayerTamedEntity != null) {PlayerTamed = PlayerTamedEntity.getUUID();}
+
+            if (chance(4) && PlayerTamed == player.getUUID()) {
+                final float wolfHp = cat.getHealth();
+                float maxHealth = cat.getMaxHealth();
+                float futureHp = wolfHp + 1f;
+                if (maxHealth < futureHp) {futureHp = maxHealth; }
+                cat.setHealth(futureHp);
+                showParticles(cat, serverWorld, 1);
             }
         }
     }
